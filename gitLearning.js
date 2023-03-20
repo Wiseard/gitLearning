62d107f54b6099d088dce9d9f8bc2149e729f02b
Florencio <florentbaleinier@gmail.com>
2023-03-20 18:12:25 +0100

    adding html, css, js files

---
+++ gitLearning.js
@@ -0,0 +1,40 @@
+/*
+Terminal Commands :
+
+
+Vérifier les paramètres git :
+git config --list
+
+-------------
+
+Initialiser un dossier GIT :
+
+cd Documents/myFile
+git init
+
+Indexer vos fichiers html, css, js... :
+git add gitlearning.html gitlearning.css gitlearning.js
+
+-------------
+
+Créer une nouvelle version des fichiers indexés (archiver votre projet)
+git commit -m “Ajout des fichiers html, css, et js de base”
+
+-------------
+
+Configurer l'URL du repository dans le dépôt distant (github)
+git remote add 
+origin URL-du-repository.git
+
+Relier le dépôt local au dépôt distant :
+git branch -M main
+
+Envoyer nos fichiers (commit) sur le dépôt distant (github)voyer 
+git push -u origin main
+
+
+
+
+
+*/
+
