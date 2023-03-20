0cf8934d6220e2c45ad24275ea8de4e64073fbd3
Florencio <florentbaleinier@gmail.com>
2023-03-20 20:11:44 +0100

    js comment update and adding file txt

--- gitLearning.js
+++ gitLearning.js
@@ -1,6 +1,10 @@
 /*
 Terminal Commands :
 
+COMMANDES DE BASE :
+
+Créer un fichier
+touch myFile
 
 Vérifier les paramètres git :
 git config --list
@@ -32,7 +36,29 @@ git branch -M main
 Envoyer nos fichiers (commit) sur le dépôt distant (github)voyer 
 git push -u origin main
 
+-----------------------------------------------------------------
 
+BRANCHES
+
+Connaître les branches présentes dans le projet :
+git branch => l'étoile signifie que c'est la branche dans laquelle on se situe
+
+Créer une branche :
+git branch myBranch
+
+Sélectionner la branche dans laquelle on souhaite se situer :
+git checkout myBranch
+
+Créer un commit de nos fichiers présents dan sla branche :
+git commit -m "alternative project"
+
+Envoyer la branche au dépôt distant :
+push origin myBranch
+
+Fusionner une branche avec une autre :
+Cette commande doit s'utiliser à partir de la branche dans laquelle nous voulons apporter les évolutions :
+git checkout main => on sélectionner la branche "hôte"
+git merge myBranch
 
 
 
